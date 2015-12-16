2366e6fa4ea55d834517535d94d092707ed96903
Arthur White <arthur@whitedevops.com>
2015-12-16 14:22:52 +0100

    Add Exists and Upload funcs comment

--- client.go
+++ client.go
@@ -84,6 +84,9 @@ func (c *Client) List() ([]string, error) {
 }
 
 // Exists checks if the object exists in the container.
+//
+// CURL equivalent:
+// 	curl https://storage.$REGION.cloud.ovh.net/v1/AUTH_$TENANTID/$CONTAINER -X HEAD -H "X-Auth-Token: $TOKEN"
 func (c *Client) Exists(object string) (bool, error) {
 	r, err := c.request("HEAD", object, nil)
 	if err != nil {
@@ -99,7 +102,7 @@ func (c *Client) Exists(object string) (bool, error) {
 // Upload puts a new object in the container.
 //
 // CURL equivalent:
-// 	curl https://storage.$REGION.cloud.ovh.net/v1/AUTH_$TENANTID/$CONTAINER/$OBJECT -X PUT -H "X-Auth-Token: $TOKEN"
+// 	curl https://storage.$REGION.cloud.ovh.net/v1/AUTH_$TENANTID/$CONTAINER/$OBJECT -X PUT -H "X-Auth-Token: $TOKEN" --d @$FILE
 func (c *Client) Upload(object string, body io.Reader) error {
 	r, err := c.request("PUT", object, body)
 	if err != nil {
